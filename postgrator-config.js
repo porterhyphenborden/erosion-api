3bc90ad77ec240a580e34647a9cc2975dc2597ab
Callie Porter-Borden <porterhyphenborden@gmail.com>
2019-10-01 22:00:17 -0400

    updated config DB

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,8 +4,8 @@ module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DB_URL
-    : process.env.DB_URL,
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
   "host": process.env.MIGRATION_DB_HOST,
   "port": process.env.MIGRATION_DB_PORT,
   "database": process.env.MIGRATION_DB_NAME,
